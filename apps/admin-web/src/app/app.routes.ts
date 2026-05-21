71d59ffb75520e8490a1e257425bd788aa478c91
jerh011 <jeelrohe@gmail.com>
2026-05-21 12:51:06 -0700

    ruttar relativas

--- apps/admin-web/src/app/app.routes.ts
+++ apps/admin-web/src/app/app.routes.ts
@@ -3,6 +3,11 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   {
     path: '',
+    pathMatch: 'full',
+    redirectTo: 'catalogs',
+  },
+  {
+    path: '',
     loadComponent: () =>
       import('@core/layouts/main-layout/main-layout').then((m) => m.MainLayout),
     children: [
@@ -12,10 +17,5 @@ export const routes: Routes = [
           import('@catalogs/catalogs.routes').then((m) => m.catalogRoutes),
       },
     ],
-  },
-  {
-    path: '',
-    pathMatch: 'full',
-    redirectTo: 'catalogs',
   },
 ];
